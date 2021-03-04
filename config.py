b36cbd6d643922db1e7ac55cfe7932d88dd4e5c1
ReginaWangui <reginahwangui801@gmail.com>
2021-03-04 11:52:26 +0300

    added

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Access@localhost/jefferson'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Access@localhost/regina'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST = 'app/static/photos' 
     # email configuration
